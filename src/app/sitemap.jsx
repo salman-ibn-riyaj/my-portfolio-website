874041f761375396cae16fdc1906e24cc9df36e9
salman <salmanibnriyaj@gmail.com>
2026-04-15 23:03:24 +0600

    sitemap created

---
+++ src/app/sitemap.jsx
@@ -0,0 +1,11 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://salmanshah.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    
+  ];
+}
